982f8b021d7b9c3f005cd058f9a542f14b8e0059
Fabian Wurdack <fa-wurdack@t-online.de>
2024-07-18 10:14:08 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGameGrid();
+            InitializeGameElements();
+            
+        }
+
+        private void InitializeGameGrid()
+        {
+            //Define the number of rows and columns
+
+            int rows = 22;
+            int columns = 40;
+
+            //Add columns to the grid
+            for (int i = 0; i < columns; i++)
+            {
+                GameArea.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            //Add rows to the grid
+            for (int i = 0;i < rows; i++)
+            {
+                GameArea.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        private void InitializeGameElements()
+        {
+            //Initialize the snake head
+
+            SnakeHead = new Ellipse
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Green
+            };
+            
+            //Initialize the food 
+        }
+    }
+}
